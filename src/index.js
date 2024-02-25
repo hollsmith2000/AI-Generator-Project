6568ba90eccfd5f215d912bb8b946e0d13d01b9c
Holly Smith <hollsmith2002@hotmail.co.uk>
2024-02-25 23:25:15 +0000

    Change the AI from being a poem generator to a Sports car recommendation Generator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-function displayPoem(response) {
-  new Typewriter("#poem", {
+function displayCar(response) {
+  new Typewriter("#car", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
@@ -7,23 +7,24 @@ function displayPoem(response) {
   });
 }
 
-function generatePoem(event) {
+function generateCar(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "5cdbe3e5o1ab49bf1ed39fb1d03ec3ct";
-  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let prompt = `User instructions: Recommend three cars that have the characteristics${instructionsInput.value}`;
   let context =
-    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br /> element. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with `sheCodes AI` inside a <strong> element at the end of the poem";
+    //You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br /> element. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with `sheCodes AI` inside a <strong> element at the end of the poem//
+    `You are a renowned car salesman that loves sport cars. Your mission is to name three sports cars that would be suitable for the user in basic HTML. Bullet Point your three results abd seperate with a <br /> element. YOU MUST NOT provide a description of the car, ONLY the name. Make sure to follow the user instructions.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let poemElement = document.querySelector("#poem");
+  let poemElement = document.querySelector("#car");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">Generating a poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">Generating the recommendations that have the requirements: ${instructionsInput.value}</div>`;
 
-  console.log("Generating Poem");
-  axios.get(apiUrl).then(displayPoem);
+  console.log("Generating Recommendations");
+  axios.get(apiUrl).then(displayCar);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let carFormElement = document.querySelector("#car-generator-form");
+carFormElement.addEventListener("submit", generateCar);
