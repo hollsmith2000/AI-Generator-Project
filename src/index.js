b2cb4dbfa0323bc66ecf1d679374f6132ede5aaf
Holly Smith <hollsmith2002@hotmail.co.uk>
2024-02-25 14:30:09 +0000

    HTML, JavaScript & CSS User-friendly amendments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 2,
+    delay: 1,
     cursor: "",
   });
 }
@@ -16,6 +16,10 @@ function generatePoem(event) {
   let context =
     "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br /> element. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with `sheCodes AI` inside a <strong> element at the end of the poem";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">Generating a poem about ${instructionsInput.value}</div>`;
 
   console.log("Generating Poem");
   axios.get(apiUrl).then(displayPoem);
